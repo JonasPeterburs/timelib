7c2a42c834b54134d7d9eba35f3bcc6ab31d8d34
ifa92.petejona <j.peterburs@outlook.de>
2020-01-28 11:20:02 +0100

    Upload current work

--- main.c
+++ main.c
@@ -16,6 +16,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+  Beschreibung:
+  Die Funktion berechnet für ein gegebenes Datum des gregorianischen Kalenders bestehend aus Tag,
+  Monat und Jahr die Nummer des Tages, gezählt von Jahresbeginn (1. Januar) an.
+  Schaltjahre werden bei der Berechnung berücksichtigt.
+  Ist das übergebene Datum ungültig, beträgt der Rückgabewert -1.
+
+  Rückgabewert:
+  Nummer des Tages: int
+*/
+int day_of_the_year(int day, int month, int year)
+{
+    int totalDays = 0;
+
+    // continue here
+
+    // Addiert alle Monate zusammen, welche vor dem Angegebenden Moant sind.
+    for (int i = 1; i < month; i++)
+    {
+        totalDays += daysInYear[i];
+    }
+
+    // Ausgabe des Ergebnisses
+    printf("Der %i.%i.%i ist der %i Tag im Jahr.\n", day, month, year, totalDays += day);
+    return 0;
+}
+
+/*
+  Beschreibung:
+  Die Funktion überprüft, ob ein gegebenes Jahr nach den Regeln des
+  gregorianischen Kalender ein Schaltjahr ist.
+  Bei Jahreszahlen vor dem Jahr 1582 wird ein Fehler zurückgegeben.
+
+  Rückgabewert:
+  1, wenn übergebenes Jahr ein Schaltjahr ist
+  0, wenn übergebenes Jahr kein Schaltjahr ist
+  -1, wenn ein ungültiges Jahr übergeben wurde
+*/
+int is_leapyear(int year)
+{
+    if (year < 1582) return -1;
+    else if ((year%4 == 0 && year%100 != 0) || year%400 == 0) { return 1; }
+    else { return 0; }
+}
+
+/*
+  Beschreibung:
+  Die Funktion bestimmt für einen gegebenen Monat eines gegebenen Jahres,
+  wie viele Tage der Monat hat. Der Wert des Monats muss zwischen 1 und 12 liegen.
+  Schaltjahre werden berücksichtigt.
+
+  Rückgabewert:
+  Anzahl von Tagen: int, 1-31, wenn ein gültiger Monar eingegeben wurde
+  -1, wenn ein ungültiger Monat oder Jahr übergeben wurde
+*/
+int get_days_for_month(int month, int year)
+{
+    int daysInYear[13] = {0, 31,28,31,30,31,30,31,31,30,31,30,31};
+
+    if (month < 1 || month > 12) { return -1; }
+
+    if (is_leapyear(year)) { daysInYear[2] = 29; }
+    else { daysInYear[2] = 28; }
+
+    return daysInYear[month];
+}
+
+/*
+  Beschreibung:
+  Die Funktion überprüft, ob ein eingegebenes Datum gültig ist.
+  Daten vor dem 1.1.1582 sind ungültig, genauso wie alle Daten nach dem 31.12.2400.
+
+  Rückgabewert:
+  1, wenn das übergebene Datum gültig ist
+  0, wenn das Datum nicht gültig ist
+*/
+int exists_date(int day, int month, int year)
+{
+    if (day < 1 || month < 1 || year < 1582 || day > 31 || month > 12 || year > 2400) return 0;
+    else if (month = 2 && day > 29) { return 0; }
+    else { return 1; }
+}
+
+
+// ------------------------------------------------------------
 int validateDate(int day, int month, int year, int daysInYear[]);
 int checkLeapYear(int year);
 int sendResult(int day, int month, int year, int daysInYear[]);
@@ -23,7 +108,7 @@ int sendResult(int day, int month, int year, int daysInYear[]);
 int main()
 {
     int day, month, year;
-    int daysInYear[13] = {0, 31,28,31,30,31,30,31,31,30,31,30,31};
+
 
     // Eingabe der Werte
     printf("Tool zur berechnung des Tages im Jahr.\n");
@@ -40,6 +125,8 @@ int main()
     scanf("%i", &year);
     fflush(stdin);
 
+
+
     validateDate(day, month, year, daysInYear);
 
     sendResult(day, month, year, daysInYear);
@@ -47,62 +134,9 @@ int main()
     return 0;
 }
 
-/*
-  Validirung des Tages, Monats und Jahres (nicht negativ oder 0)
-  Tag nicht über 31 und nicht über Monatsgrenze
-  Monat nicht über 12
-*/
-int validateDate(int day, int month, int year, int daysInYear[])
-{
-    // Validirung des Tages, Monats und Jahres (nicht negativ oder 0)
-    if (day <= 0 || month <= 0 || year <= 0 || day > 31 || month > 12)
-    {
-        printf("Error! Werte unrealistisch.\n");
-        printf("Bitte überprüfen Sie ihre Eingabe. (got: %i.%i.%i)\n", day, month, year);
-        return 0;
-    }
 
-    // Überprüung auf Schaltjahr wenn der angegebende Monat 02 entspricht (Februar)
-    if (month >= 2)
-    {
-        if (checkLeapYear(year))
-        {
-            daysInYear[2] = 29;
-        }
-    }
 
-    // Überprüfung ob Tag im Monat moglich ist
-    if (day > daysInYear[month])
-    {
-        return 0;
-    }
-    return 0;
-}
 
-/*
-  Der Gregoranischer Kalender gibt 3 Regeln an:
-  1. Die durch 4 ganzzahlig teilbaren Jahre sind Schaltjahre.
-  2. Säkularjahre, also die Jahre, die ein Jahrhundert
-     abschließen (z. B. 1800, 1900, 2100 und 2200)
-     sind keine Schaltjahre.
-  3. Die durch 400 ganzzahlig teilbaren S�kularjahre
-     doch Schaltjahre.
-
-  Quelle: https://de.wikipedia.org/wiki/Schaltjahr
-*/
-int checkLeapYear(int year)
-{
-    if ((year%4 == 0 && year%100 != 0) || year%400 == 0)
-    {
-        printf("Jahr ist ein Schaltjahr");
-        return 1;
-    }
-    else
-    {
-        printf("Jahr ist kein Schaltjahr");
-        return 0;
-    }
-}
 
 /*
   Bereitstellung und ausgabe der Werte
