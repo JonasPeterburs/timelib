d98be133220a55d1204e7a8be99d49bbf708196c
ifa92.petejona <j.peterburs@outlook.de>
2020-02-11 09:13:06 +0100

    Add A-Style comments

--- main.c
+++ main.c
@@ -1,34 +1,34 @@
-/*
-  Schreiben Sie ein Programm, welches nach Eingabe des aktuellen Datums berechnet,
-  der wievielte Tag in diesem Jahr es ist.
-
-  int tage[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-
-  Eingabegrößen: Tag, Monat, Jahr
-  Ausgabegrößen: Nummer des Tages
-
-  Hinweis: Es soll bei Eingabe überprüft werden, ob der eingegebene Tag existiert.
-  Schaltjahre soll ebenfalls berücksichtigt werden!
-
-  @author Jonas Peterburs
-  @date 14-01-2020
-*/
+/* *
+ * Schreiben Sie ein Programm, welches nach Eingabe des aktuellen Datums berechnet,
+ * der wievielte Tag in diesem Jahr es ist.
+ *
+ * int tage[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+ *
+ * Eingabegrößen: Tag, Monat, Jahr
+ * Ausgabegrößen: Nummer des Tages
+ *
+ * Hinweis: Es soll bei Eingabe überprüft werden, ob der eingegebene Tag existiert.
+ * Schaltjahre soll ebenfalls berücksichtigt werden!
+ *
+ * Author: Jonas Peterburs
+ * Datum: 14.01.2020
+ * */
 #include <stdio.h>
 #include <stdlib.h>
 
 #define DATE_ERROR "\nERROR : Datum ungültig.\n\n"
 
 
-/*
-  Beschreibung:
-  Die Funktion berechnet für ein gegebenes Datum des gregorianischen Kalenders bestehend aus Tag,
-  Monat und Jahr die Nummer des Tages, gezählt von Jahresbeginn (1. Januar) an.
-  Schaltjahre werden bei der Berechnung berücksichtigt.
-  Ist das übergebene Datum ungültig, beträgt der Rückgabewert -1.
-
-  Rückgabewert:
-  Nummer des Tages: int
-*/
+/* *
+ * Beschreibung:
+ * Die Funktion berechnet für ein gegebenes Datum des gregorianischen Kalenders bestehend aus Tag,
+ * Monat und Jahr die Nummer des Tages, gezählt von Jahresbeginn (1. Januar) an.
+ * Schaltjahre werden bei der Berechnung berücksichtigt.
+ * Ist das übergebene Datum ungültig, beträgt der Rückgabewert -1.
+ *
+ * Rückgabewert:
+ * Nummer des Tages: int
+ * */
 int day_of_the_year(int day, int month, int year)
 {
     int totalDays = 0;
@@ -49,10 +49,14 @@ int day_of_the_year(int day, int month, int year)
     return totalDays;
 }
 
-/*
-  Die Funktion liest 3 Ganzzahlwerte (Integer) ein, für Tag, Monat und Jahr.
-  Wenn das angegebene Datum ungültig ist, wird erneut eingelesen, solange bis ein gültiges Datum eingegeben wurde.
-*/
+/* *
+ * Beschreibung:
+ * Die Funktion liest 3 Ganzzahlwerte (Integer) ein, für Tag, Monat und Jahr.
+ * Wenn das angegebene Datum ungültig ist, wird erneut eingelesen, solange bis ein gültiges Datum eingegeben wurde.
+ *
+ * Rückgabewert:
+ * kein Rückgabewert
+ * */
 int input_date(int *day, int *month, int *year)
 {
     // Eingabe der Werte
@@ -60,6 +64,9 @@ int input_date(int *day, int *month, int *year)
 
     do
     {
+	// reset Pointers
+	*day = 0, *month = 0, *year = 0;
+
         printf("Tag: ");
         scanf("%i", day);
         fflush(stdin);
@@ -84,17 +91,17 @@ int input_date(int *day, int *month, int *year)
     while (exists_date(*day, *month, *year) != 1);
 }
 
-/*
-  Beschreibung:
-  Die Funktion überprüft, ob ein gegebenes Jahr nach den Regeln des
-  gregorianischen Kalender ein Schaltjahr ist.
-  Bei Jahreszahlen vor dem Jahr 1582 wird ein Fehler zurückgegeben.
-
-  Rückgabewert:
-  1, wenn übergebenes Jahr ein Schaltjahr ist
-  0, wenn übergebenes Jahr kein Schaltjahr ist
-  -1, wenn ein ungültiges Jahr übergeben wurde
-*/
+/* *
+ * Beschreibung:
+ * Die Funktion überprüft, ob ein gegebenes Jahr nach den Regeln des
+ * gregorianischen Kalender ein Schaltjahr ist.
+ * Bei Jahreszahlen vor dem Jahr 1582 wird ein Fehler zurückgegeben.
+ *
+ * Rückgabewert:
+ * 1, wenn übergebenes Jahr ein Schaltjahr ist
+ * 0, wenn übergebenes Jahr kein Schaltjahr ist
+ * -1, wenn ein ungültiges Jahr übergeben wurde
+ * */
 int is_leapyear(int year)
 {
     if (year < 1582)
@@ -109,16 +116,16 @@ int is_leapyear(int year)
     }
 }
 
-/*
-  Beschreibung:
-  Die Funktion bestimmt für einen gegebenen Monat eines gegebenen Jahres,
-  wie viele Tage der Monat hat. Der Wert des Monats muss zwischen 1 und 12 liegen.
-  Schaltjahre werden berücksichtigt.
-
-  Rückgabewert:
-  Anzahl von Tagen: int, 1-31, wenn ein gültiger Monar eingegeben wurde
-  -1, wenn ein ungültiger Monat oder Jahr übergeben wurde
-*/
+/* *
+ * Beschreibung:
+ * Die Funktion bestimmt für einen gegebenen Monat eines gegebenen Jahres,
+ * wie viele Tage der Monat hat. Der Wert des Monats muss zwischen 1 und 12 liegen.
+ * Schaltjahre werden berücksichtigt.
+ *
+ * Rückgabewert:
+ * Anzahl von Tagen: int, 1-31, wenn ein gültiger Monar eingegeben wurde
+ * -1, wenn ein ungültiger Monat oder Jahr übergeben wurde
+ * */
 int get_days_for_month(int month, int year)
 {
     int daysInYear[13] = {0, 31,28,31,30,31,30,31,31,30,31,30,31};
@@ -142,13 +149,13 @@ int get_days_for_month(int month, int year)
 }
 
 /*
-  Beschreibung:
-  Die Funktion überprüft, ob ein eingegebenes Datum gültig ist.
-  Daten vor dem 1.1.1582 sind ungültig, genauso wie alle Daten nach dem 31.12.2400.
-
-  Rückgabewert:
-  1, wenn das übergebene Datum gültig ist
-  0, wenn das Datum nicht gültig ist
+ * Beschreibung:
+ * Die Funktion überprüft, ob ein eingegebenes Datum gültig ist.
+ * Daten vor dem 1.1.1582 sind ungültig, genauso wie alle Daten nach dem 31.12.2400.
+ *
+ * Rückgabewert:
+ * 1, wenn das übergebene Datum gültig ist
+ * 0, wenn das Datum nicht gültig ist
 */
 int exists_date(int day, int month, int year)
 {
