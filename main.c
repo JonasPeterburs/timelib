be2f55add3c8ee107bb891668f38a3d7c36032c8
Jonas <j.peterburs@outlook.de>
2020-05-05 10:26:02 +0200

    Add inline docs to example

--- main.c
+++ main.c
@@ -11,8 +11,11 @@ int main()
 {
     printf("Calculation of the day of the year.\n\n");
 
+    // set user input to date struct
     struct date current_date = input_date();
 
+    // returns example
+    // format: day.month.year --> day_of_the_year
     printf("%i.%i.%i --> %i\n", current_date.day, current_date.month, current_date.year, day_of_the_year(current_date));
     return 0;
 }
